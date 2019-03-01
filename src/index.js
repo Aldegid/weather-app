d371bfc3fffd8de291abe41d8e089f95684fc3c2
Aldegid <aldegid@gmail.com>
2019-03-01 23:58:50 +0200

    add api key

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ const favorite = document.querySelector('.fav-button');
 favorite.addEventListener('click', function(){
   favorite.classList.toggle('active');
 })
+
+const api = `http://api.openweathermap.org/data/2.5/find?q=Kyiv&units=metric&appid=ffa19df254477b52e5f0e38980606a54`
+fetch(api)
+  .then((res) => res.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
